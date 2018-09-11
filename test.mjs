20c76370e041bd3127806582a3be49b56570b00f
59naga <i59naga@icloud.com>
2018-09-11 15:27:25 +0900

    :wrench: upgrade the devDeps

--- test.mjs
+++ test.mjs
@@ -1,13 +1,12 @@
-import spec from "eastern";
 import { strictEqual } from "assert";
 import namedImport from "./";
 
-spec("should import named default export", async () => {
+it("should import named default export", async () => {
   const fixture = await namedImport("./fixture");
   strictEqual(fixture, "foo");
 });
 
-spec("should import named other export", async () => {
+it("should import named other export", async () => {
   const fixture = await namedImport("./fixture", "other");
   strictEqual(fixture, "bar");
 });
